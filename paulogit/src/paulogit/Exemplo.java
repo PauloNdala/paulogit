5c9649b7ffc9c32cc796792f73b7094ab38b918b
Paulo Ndumba Ndala <87448044+PauloNdala@users.noreply.github.com>
2022-03-07 19:54:48 +0000

    Update Exemplo.java

--- paulogit/src/paulogit/Exemplo.java
+++ paulogit/src/paulogit/Exemplo.java
@@ -4,7 +4,8 @@ public class Exemplo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ol� tudo bem");
+		System.out.println("Olá tudo bem");
+		System.out.println("Olá tudo bem");
 
 	}
 
